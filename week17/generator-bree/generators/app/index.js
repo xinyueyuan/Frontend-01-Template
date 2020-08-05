455cead84bd00ca760f0da3368c404639297af58
yxy <>
2020-08-05 13:07:04 +0800

    generator

---
+++ week17/generator-bree/generators/app/index.js
@@ -0,0 +1,36 @@
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  // The name `constructor` is important here
+  constructor(args, opts) {
+    // Calling the super constructor is important so our generator is correctly set up
+    super(args, opts);
+
+    // Next, add your custom code
+    this.option('babel'); // This method adds support for a `--babel` flag
+  }
+
+  // method1() {
+  //   this.log('method 1 just ran');
+  // }
+
+  // method2() {
+  //   this.log('method 2 just ran');
+  // }
+
+  async prompting() {
+    this.answers = await this.prompt([{
+      type    : 'input',
+      name    : 'title',
+      message : 'Your project title',
+    }]);
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('public/index.html'),
+      { title: 'Templating with Yeoman' }
+    );
+  }
+};
