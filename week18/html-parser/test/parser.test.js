bd6f4ca44e5b4a10177c2f1d944a2548628d834a
昕昕 <>
2020-08-12 15:21:00 +0800

    week 18

--- week18/html-parser/test/parser.test.js
+++ week18/html-parser/test/parser.test.js
@@ -35,7 +35,107 @@ it('text with <', function () {
 });
 
 it('with property', function () {
-  let doc = parseHTML("<div id='a'></div>"); 
-  let div = doc.children
-  console.log(div)
+  let doc = parseHTML("<div id=a class='cls' data=\"abc\" ></div>"); 
+  let div = doc.children[0]
+  let count = 0
+  for(let attr of div.attributes) {
+    if(attr.name === "id") {
+      assert.equal(attr.value, "a");
+      count++;
+    }
+    if(attr.name === "class") {
+      assert.equal(attr.value, "cls");
+      count++;
+    }
+    if(attr.name === "data") {
+      assert.equal(attr.value, "abc");
+      count++;
+    }
+  }
+  assert.equal(count, 3);
 });
+
+it('with property 2', function () {
+  let doc = parseHTML("<div id=a class='cls' data=\"abc\"></div>"); 
+  let div = doc.children[0]
+  let count = 0
+  for(let attr of div.attributes) {
+    if(attr.name === "id") {
+      assert.equal(attr.value, "a");
+      count++;
+    }
+    if(attr.name === "class") {
+      assert.equal(attr.value, "cls");
+      count++;
+    }
+    if(attr.name === "data") {
+      assert.equal(attr.value, "abc");
+      count++;
+    }
+  }
+  assert.equal(count, 3);
+});
+
+it('with property 3', function () {
+  let doc = parseHTML("<div id=a class='cls' data=\"abc\"/>"); 
+  let div = doc.children[0]
+  let count = 0
+  for(let attr of div.attributes) {
+    if(attr.name === "id") {
+      assert.equal(attr.value, "a");
+      count++;
+    }
+    if(attr.name === "class") {
+      assert.equal(attr.value, "cls");
+      count++;
+    }
+    if(attr.name === "data") {
+      assert.equal(attr.value, "abc");
+      count++;
+    }
+  }
+  assert.equal(count, 3);
+});
+
+it('script', function() {
+  let content = `
+    <div>abcd</div>
+    <span>x</span>
+    <
+    <s
+    <sc
+    <scr
+    <scri
+    <scrip
+    <script
+    </s
+    </sc
+    </scr
+    </scri
+    </scrip
+    </script
+  `
+  let doc = parseHTML(`<script>${content}</script >`)
+  let text = doc.children[0].children[0];
+  assert.equal(text.content, content);
+  assert.equal(text.type, "text");
+})
+
+it('attribute with no value', function () {
+  let doc = parseHTML("<div class />");
+  let div = doc.children[0]
+  assert.equal(div.tagName, "div");
+  assert.equal(div.children.length, 0);
+  assert.equal(div.type, "element");
+});
+
+
+
+it('attribute with no value', function () {
+  let doc = parseHTML("<div class id/>");
+  let div = doc.children[0]
+  assert.equal(div.tagName, "div");
+  assert.equal(div.children.length, 0);
+  assert.equal(div.type, "element");
+});
+
